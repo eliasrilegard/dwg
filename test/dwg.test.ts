23423563ea337e0560990752c9c9f10a56fdc1c4
eliasrilegard <e_rilegard@live.se>
2022-08-01 15:58:24 +0200

    Update tests

--- test/dwg.test.ts
+++ test/dwg.test.ts
@@ -7,7 +7,6 @@ describe('Basic functionality, testing functions', () => {
   it('addVertex', done => {
     expect(graph.addVertex(2)).to.be.an.instanceof(DWG);
     expect(graph.addVertex(5)).to.be.an.instanceof(DWG);
-    expect(() => graph.addVertex(2)).to.throw('Vertex');
     done();
   });
 
@@ -66,10 +65,22 @@ describe('Basic functionality, testing functions', () => {
   });
 
   it('removeEdge', done => {
-    expect(graph.removeEdge(2, 5)).to.equal('2 -> 5');
+    expect(() => graph.removeEdge(0, 2)).to.throw('First');
+    expect(() => graph.removeEdge(2, 0)).to.throw('Second');
+    expect(graph.removeEdge(2, 5)).to.be.true;
     expect(graph.getEdges(2)).to.be.empty;
     expect(graph.vertexCount).to.equal(2);
     expect(graph.edgeCount).to.equal(1);
+    expect(graph.removeEdge(2, 5)).to.be.false;
+    done();
+  });
+
+  it('removeVertex', done => {
+    expect(graph.removeVertex(2)).to.be.true;
+    expect(graph.removeVertex(2)).to.be.false;
+    expect(graph.removeVertex(1)).to.be.false;
+    expect(graph.vertexCount).to.equal(1);
+    expect(graph.edgeCount).to.equal(0);
     done();
   });
 });
@@ -82,7 +93,18 @@ describe('Edge cases (advanced)', () => {
     graph.addVertex(2);
     graph.addEdge(1, 2, 'foo');
     graph.addEdge(1, 2, 'bar');
-    expect(graph.getEdges(1)[0]).to.have.members([2, 'bar']);
+    const edgeList = graph.getEdges(1);
+    expect(edgeList).to.be.an('array').with.lengthOf(1);
+    expect(edgeList[0]).to.have.members([2, 'bar']);
+    expect(graph.edgeCount).to.equal(1);
+    done();
+  });
+
+  it('Overwriting vertices', done => {
+    graph.addVertex(2);
+    expect(graph.getEdges(1)).to.be.empty;
+    expect(graph.edgeCount).to.equal(0);
+    expect(graph.vertexCount).to.equal(2);
     done();
   });
 });
